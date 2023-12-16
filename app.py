b2636f3d586b90efa3ed99a1a93013ce5e956714
Rajendra Vechalapu <vrsanaidu@gmail.com>
2023-12-17 10:07:26 +1300

    changes made and working fine

--- app.py
+++ app.py
@@ -1,24 +1,37 @@
 from flask import Flask, render_template, request
+from transformers import AutoTokenizer
 
 app = Flask(__name__)
 
+# Replace 'bert-base-uncased' with the specific model you are using
+tokenizer = AutoTokenizer.from_pretrained('bert-base-uncased')
+
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('index.html', content="", token_count=None)
 
 @app.route('/upload', methods=['POST'])
 def upload():
-    if 'file' not in request.files:
-        return "No file part"
+    content = ""
+    token_count = None
 
-    file = request.files['file']
+    if 'file' in request.files:
+        file = request.files['file']
+        if file.filename != '':
+            content = file.read().decode('utf-8')
+            token_count = count_tokens(content)
 
-    if file.filename == '':
-        return "No selected file"
+    if 'free_text' in request.form:
+        free_text = request.form['free_text']
+        content = free_text
+        token_count = count_tokens(free_text)
 
-    content = file.read().decode('utf-8')
+    return render_template('index.html', content=content, token_count=token_count)
 
-    return render_template('index.html', content=content)
+def count_tokens(text):
+    # Use 'return_tensors' argument to ensure the output is a tensor
+    tokens = tokenizer.encode(text, return_tensors="pt")
+    return len(tokens[0])  # Extract the number of tokens using len()
 
 if __name__ == '__main__':
     app.run(debug=True)
