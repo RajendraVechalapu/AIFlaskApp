26b49af69c53e19bd02a0e3011d764199bfa5f98
Rajendra Vechalapu <vrsanaidu@gmail.com>
2023-12-17 19:15:16 +1300

    changes sentiment analysis

--- app.py
+++ app.py
@@ -1,32 +1,39 @@
 from flask import Flask, render_template, request
+import requests
 
 app = Flask(__name__)
 
+# Hugging Face API endpoint for sentiment analysis
+HUGGINGFACE_SENTIMENT_API = "https://api-inference.huggingface.co/models/lxyuan/distilbert-base-multilingual-cased-sentiments-student"
+HUGGINGFACE_API_KEY = "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"  # Replace with your actual API key
+
+headers = {"Authorization": f"Bearer {HUGGINGFACE_API_KEY}"}
+
 @app.route('/')
 def index():
-    return render_template('index.html', content="", token_count=None)
+    return render_template('index.html', sentiment_result=None)
 
-@app.route('/upload', methods=['POST'])
-def upload():
-    content = ""
-    token_count = None
+@app.route('/analyze_sentiment', methods=['POST'])
+def analyze_sentiment():
+    sentiment_result = None
 
-    if 'file' in request.files:
-        file = request.files['file']
-        if file.filename != '':
-            content = file.read().decode('utf-8')
-            token_count = count_words(content)
+    if 'text' in request.form:
+        text = request.form['text']
+        sentiment_result = query_sentiment(text)
 
-    if 'free_text' in request.form:
-        free_text = request.form['free_text']
-        content = free_text
-        token_count = count_words(free_text)
+    return render_template('index.html', sentiment_result=sentiment_result)
 
-    return render_template('index.html', content=content, token_count=token_count)
-
-def count_words(text):
-    words = text.split()
-    return len(words)
+def query_sentiment(text):
+    try:
+        payload = {"inputs": text}
+        response = requests.post(HUGGINGFACE_SENTIMENT_API, headers=headers, json=payload)
+        response.raise_for_status()  # Check for HTTP errors
+        sentiment_result = response.json()[0]
+        return sentiment_result
+    except requests.exceptions.RequestException as e:
+        # Handle request errors gracefully
+        print(f"Error in sentiment analysis request: {e}")
+        return None  # Return None in case of an error
 
 if __name__ == '__main__':
     app.run(debug=True)
