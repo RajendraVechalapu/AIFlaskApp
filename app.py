5c403b838daafa45f09a45d00b4eae03cfa07ff3
Rajendra Vechalapu <vrsanaidu@gmail.com>
2023-12-17 13:01:30 +1300

    changes

--- app.py
+++ app.py
@@ -1,10 +1,6 @@
 from flask import Flask, render_template, request
-from transformers import AutoTokenizer
 
 app = Flask(__name__)
-
-# Replace 'bert-base-uncased' with the specific model you are using
-tokenizer = AutoTokenizer.from_pretrained('bert-base-uncased')
 
 @app.route('/')
 def index():
@@ -19,19 +15,18 @@ def upload():
         file = request.files['file']
         if file.filename != '':
             content = file.read().decode('utf-8')
-            token_count = count_tokens(content)
+            token_count = count_words(content)
 
     if 'free_text' in request.form:
         free_text = request.form['free_text']
         content = free_text
-        token_count = count_tokens(free_text)
+        token_count = count_words(free_text)
 
     return render_template('index.html', content=content, token_count=token_count)
 
-def count_tokens(text):
-    # Use 'return_tensors' argument to ensure the output is a tensor
-    tokens = tokenizer.encode(text, return_tensors="pt")
-    return len(tokens[0])  # Extract the number of tokens using len()
+def count_words(text):
+    words = text.split()
+    return len(words)
 
 if __name__ == '__main__':
     app.run(debug=True)
