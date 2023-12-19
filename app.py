223f17ff83a8e8eb7edaaced6939548edeeae779
Rajendra Vechalapu <vrsanaidu@gmail.com>
2023-12-19 20:46:36 +1300

    changes

--- app.py
+++ app.py
@@ -3,36 +3,36 @@ import requests
 
 app = Flask(__name__)
 
-# Hugging Face API endpoint for sentiment analysis
-HUGGINGFACE_SENTIMENT_API = "https://api-inference.huggingface.co/models/lxyuan/distilbert-base-multilingual-cased-sentiments-student"
-HUGGINGFACE_API_KEY = "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"  # Replace with your actual API key
+# Hugging Face API endpoint for summarization
+HUGGINGFACE_SUMMARIZATION_API = "https://api-inference.huggingface.co/models/facebook/bart-large-cnn"
+HUGGINGFACE_API_KEY = "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"  # Replace with your actual API key
 
 headers = {"Authorization": f"Bearer {HUGGINGFACE_API_KEY}"}
 
 @app.route('/')
 def index():
-    return render_template('index.html', sentiment_result=None)
+    return render_template('index.html', summary_result=None)
 
-@app.route('/analyze_sentiment', methods=['POST'])
-def analyze_sentiment():
-    sentiment_result = None
+@app.route('/summarize_text', methods=['POST'])
+def summarize_text():
+    summary_result = None
 
     if 'text' in request.form:
         text = request.form['text']
-        sentiment_result = query_sentiment(text)
+        summary_result = summarizeTextHuggingFace(text, max_summary_length=150)
 
-    return render_template('index.html', sentiment_result=sentiment_result)
+    return render_template('index.html', summary_result=summary_result)
 
-def query_sentiment(text):
+def summarizeTextHuggingFace(text, max_summary_length):
     try:
-        payload = {"inputs": text}
-        response = requests.post(HUGGINGFACE_SENTIMENT_API, headers=headers, json=payload)
+        payload = {"inputs": text, "max_summary_length": max_summary_length}
+        response = requests.post(HUGGINGFACE_SUMMARIZATION_API, headers=headers, json=payload)
         response.raise_for_status()  # Check for HTTP errors
-        sentiment_result = response.json()[0]
-        return sentiment_result
+        summary = response.json()[0]['summary_text']
+        return summary
     except requests.exceptions.RequestException as e:
         # Handle request errors gracefully
-        print(f"Error in sentiment analysis request: {e}")
+        print(f"Error in summarization request: {e}")
         return None  # Return None in case of an error
 
 if __name__ == '__main__':
