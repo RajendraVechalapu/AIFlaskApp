d2e3ee839976cfd41b25a1cc088f0118812102e9
Rajendra Vechalapu <vrsanaidu@gmail.com>
2023-12-29 18:01:17 +1300

    further changes

--- main.py
+++ main.py
@@ -1,7 +1,14 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, File, UploadFile
+from fastapi.responses import HTMLResponse
+from fastapi.middleware.cors import CORSMiddleware
+from typing import Optional
+import fitz  # PyMuPDF
+from fastapi.templating import Jinja2Templates
+from starlette.requests import Request
+import requests
 
 app = FastAPI()
 
 @app.get("/")
 def read_root():
-    return {"message": "Hello World, Rajendra! How are you?"}
+    return {"message": "Hello World, Rajendra Simhadri Appala Naidu Vechalapu! First App?"}
