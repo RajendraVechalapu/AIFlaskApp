8ab40ca7819920777e71db2d026b184c7549112b
Rajendra Vechalapu <vrsanaidu@gmail.com>
2023-12-29 18:48:54 +1300

    changes

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Form, Request
+from fastapi.responses import HTMLResponse
 import requests
 
 app = FastAPI()
@@ -8,21 +9,18 @@ HUGGINGFACE_SUMMARIZATION_API = "https://api-inference.huggingface.co/models/fac
 headers = {"Authorization": f"Bearer {HUGGINGFACE_API_KEY}"}
 
 @app.get("/")
-def generate_summary():
+def get_main_page(request: Request):
+    return HTMLResponse(content=open("templates/index.html").read(), status_code=200)
+
+@app.post("/summarize")
+def generate_summary(text: str = Form(...)):
     try:
-        payload = {
-            "inputs": "wonderful",
-        }
+        payload = {"inputs": text}
         response = requests.post(HUGGINGFACE_SUMMARIZATION_API, headers=headers, json=payload)
-        if response.status_code == 200 and isinstance(response.json(), list) and response.json():
-            summary = response.json()[0].get('summary_text', "")
-            
-            # Replace escaped double quotes with regular quotes
-            summary = summary.replace('\\"', '"')
-            
-            # Format as bullet points without outer braces
-            formatted_response = f'1. {summary.replace(".", ".\n2. ")}'
-            return formatted_response
+        if response.status_code == 200:
+            summary_text = response.json()[0].get('summary_text', "")
+            formatted_summary = f"{summary_text.replace('.', '.\n')}"
+            return HTMLResponse(content=formatted_summary, status_code=200)
         else:
             return {"error": f"Error: Unable to summarize. Status code {response.status_code}"}
     except Exception as e:
