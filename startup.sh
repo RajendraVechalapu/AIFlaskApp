451558ed933515a6e71dcb9ccf5cc07ffc1559d8
Rajendra Vechalapu <vrsanaidu@gmail.com>
2023-12-29 17:53:05 +1300

    changes

--- startup.sh
+++ startup.sh
@@ -1,10 +1,1 @@
-#!/bin/bash
-
-# Create a new virtual environment
-python -m venv rajenv
-
-# Activate the virtual environment
-source rajenv/bin/activate
-
-# Install dependencies from requirements.txt
-pip install -r requirements.txt
+python -m uvicorn main:app --host 0.0.0.0 --port 80
