1e6369bd7b03e98b977169fb26ce1678740f29f9
Rajendra Vechalapu <vrsanaidu@gmail.com>
2023-12-29 18:31:18 +1300

    changtes

--- main.py
+++ main.py
@@ -1,47 +1,29 @@
-from fastapi import FastAPI, File, UploadFile, Form
-from fastapi.responses import HTMLResponse
-from fastapi.middleware.cors import CORSMiddleware
-from fastapi.templating import Jinja2Templates
-from starlette.requests import Request
+from fastapi import FastAPI
 import requests
 
 app = FastAPI()
 
-# Enable CORS (customize origins as needed)
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=["*"],
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
-
-# Setup templates
-templates = Jinja2Templates(directory="templates")
-
-HUGGINGFACE_API_KEY = "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"  # Replace with your key
+HUGGINGFACE_API_KEY = "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"
 HUGGINGFACE_SUMMARIZATION_API = "https://api-inference.huggingface.co/models/facebook/bart-large-cnn"
 headers = {"Authorization": f"Bearer {HUGGINGFACE_API_KEY}"}
 
-class ChatSummarizer:
-    def __init__(self):
-        pass
-
-    def generate_summary(self, text: str) -> str:
-        payload = {"text": text}
-        response = requests.post(HUGGINGFACE_SUMMARIZATION_API, headers=headers, json=payload)
-        if response.status_code == 200:
-            return response.json().get("summary", "")
-        else:
-            return f"Error: Unable to summarize. Status code {response.status_code}"
-
-chat_summarizer = ChatSummarizer()
-
 @app.get("/")
-async def upload_form(request: Request):
-    return templates.TemplateResponse("index.html", {"request": request})
-
-@app.post("/generate_summary")
-async def generate_summary(text: str = Form(...)):
-    summary = chat_summarizer.generate_summary(text)
-    return {"text": text, "summary": summary}
+def generate_summary():
+    try:
+        payload = {
+            "inputs": "wonderful",
+        }
+        response = requests.post(HUGGINGFACE_SUMMARIZATION_API, headers=headers, json=payload)
+        if response.status_code == 200 and isinstance(response.json(), list) and response.json():
+            summary = response.json()[0].get('summary_text', "")
+            
+            # Replace escaped double quotes with regular quotes
+            summary = summary.replace('\\"', '"')
+            
+            # Format as bullet points without outer braces
+            formatted_response = f'1. {summary.replace(".", ".\n2. ")}'
+            return formatted_response
+        else:
+            return {"error": f"Error: Unable to summarize. Status code {response.status_code}"}
+    except Exception as e:
+        return {"error": str(e)}
