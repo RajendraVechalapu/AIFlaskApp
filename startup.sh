2d0c7b67136b7dadb7cb783bec53a37902a0001c
Rajendra Vechalapu <vrsanaidu@gmail.com>
2023-12-31 10:25:44 +1300

    commit changes

--- startup.sh
+++ startup.sh
@@ -1,6 +1,10 @@
 <<<<<<< HEAD
+<<<<<<< HEAD
 # python -m uvicorn main:app --host 0.0.0.0 --port 80
 python -m spacy download en_core_web_sm
+=======
+python -m uvicorn main:app --host 0.0.0.0 --port 80
+>>>>>>> parent of 2807fdc (Merge pull request #6 from RajendraVechalapu/fasiapi---changes)
 =======
 python -m uvicorn main:app --host 0.0.0.0 --port 80
 >>>>>>> parent of 2807fdc (Merge pull request #6 from RajendraVechalapu/fasiapi---changes)
